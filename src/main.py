2be0b115935ae2051f5ee632a0ef76854745cf21
Denis <den23208@mail.ru>
2024-11-27 09:38:22 +0500

    problem flake8

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.widget import get_mask_card_number, get_data
 
-get_mask_card_number('Maestro 7000792289606361')
+print(get_mask_card_number("Maestro 7000792289606361"))
 # print(get_mask_check_card("Счет 736541084301358745"))
 
-get_data('"2024-03-11T02:26:18.671407"')
+print(get_data('"2024-03-11T02:26:18.671407"'))
--- src/widget.py
+++ src/widget.py
@@ -4,21 +4,35 @@ def get_mask_card_number(num_card: str) -> str:
     list_card = []
     for i in num_card:
         list_card.append(i)
-    one_space = list_card.index(' ')
+    one_space = list_card.index(" ")
     name_type_card = list_card[:one_space]
-    if ''.join(name_type_card) == 'Счет':
-        print(''.join(name_type_card) + ' **' + num_card[-4:])
-    elif ''.join(name_type_card) == 'Maestro':
-        print(''.join(name_type_card) + num_card[one_space:one_space + 5] + ' ' + num_card[
-                                                                                  one_space + 5:one_space + 7] + '** ****' + num_card[
-                                                                                                                             -4:])
-    elif ''.join(name_type_card) == 'Visa':
-        print(''.join(name_type_card) + num_card[one_space:one_space + 9] + num_card[
-                                                                            one_space + 9:one_space + 14] + ' ' + num_card[
-                                                                                                                  one_space + 14:one_space + 16] + '** ****' + num_card[
-                                                                                                                                                               -4:])
+    if "".join(name_type_card) == "Счет":
+        return "".join(name_type_card) + " **" + num_card[-4:]
+    elif "".join(name_type_card) == "Maestro":
+        return (
+            "".join(name_type_card)
+            + num_card[one_space : one_space + 5]
+            + " "
+            + num_card[one_space + 5 : one_space + 7]
+            + "** ****"
+            + num_card[-4:]
+        )
+
+    elif "".join(name_type_card) == "Visa":
+        return (
+            "".join(name_type_card)
+            + num_card[one_space : one_space + 9]
+            + num_card[one_space + 9 : one_space + 14]
+            + " "
+            + num_card[one_space + 14 : one_space + 16]
+            + "** ****"
+            + num_card[-4:]
+        )
+    else:
+        return "Номер карты неизвестен"
+
 
 def get_data(info_clock: str) -> str:
     """Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
-     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")."""
-    print(info_clock[9:11] + '.' + info_clock[6:8] + '.' + info_clock[1:5])
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")."""
+    return info_clock[9:11] + "." + info_clock[6:8] + "." + info_clock[1:5]
