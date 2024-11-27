b9820d77bda4d158c61b635a7d9989b6af2a5a32
Denis <den23208@mail.ru>
2024-11-27 13:56:10 +0500

    black/flake8

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import random
 from src.widget import mask_account_card, get_data
-unique_list = ['Счет 73654108430135874305', 'Visa Platinum 7000792289606361', 'Maestro 7000792289606361']
-print(mask_account_card(random.choice(unique_list)))
-print(get_data('2024-03-11T02:26:18.671407'))
 
+unique_list = ["Счет 73654108430135874305", "Visa Platinum 7000792289606361", "Maestro 7000792289606361"]
+print(mask_account_card(random.choice(unique_list)))
+print(get_data("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,19 @@
 from src.masks import get_mask_card_number, get_mask_account
+
 """Функция которая умеет обрабатывать информацию как о картах, так и о счетах"""
+
+
 def mask_account_card(card_info: str) -> str:
-    one_space = card_info.index(' ')
-    if card_info[:one_space] == 'Maestro':
+    one_space = card_info.index(" ")
+    if card_info[:one_space] == "Maestro":
         return card_info[:one_space] + get_mask_card_number(card_info[one_space:])
-    elif card_info[:one_space] == 'Visa':
-        return card_info[:one_space + 9] + get_mask_card_number(card_info[one_space + 9:])
-    elif card_info[:one_space] == 'Счет':
-        return card_info[:one_space] + ' ' + get_mask_account(card_info[one_space:])
+    elif card_info[:one_space] == "Visa":
+        return card_info[: one_space + 9] + get_mask_card_number(card_info[one_space + 9 :])
+    elif card_info[:one_space] == "Счет":
+        return card_info[:one_space] + " " + get_mask_account(card_info[one_space:])
     else:
-        return 'Нет такого типа карты'
+        return "Нет такого типа карты"
+
 
 def get_data(data: str) -> str:
     date_str = data.split("T")[0]
