0cde5ee8d9d93416485fcdb3cc042e5379f9b45a
Denis <den23208@mail.ru>
2024-11-28 12:14:06 +0500

    correct

--- src/main.py
+++ src/main.py
@@ -5,3 +5,4 @@ from src.widget import get_data, separate_digits_and_letters
 unique_list = ["Счет 73654108430135874305", "Visa Platinum 7000792289606361", "Maestro 7000792289606361"]
 print(*separate_digits_and_letters(random.choice(unique_list)))
 print(get_data("2024-03-11T02:26:18.671407"))
+
