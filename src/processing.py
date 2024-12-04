36ec0c7cf53863038fc7c853560169f6c3bfcf1d
Denis <den23208@mail.ru>
2024-12-04 23:32:53 +0500

    add funct filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+﻿from typing import List, Dict
+
+
+def filter_by_state(find_state: List[Dict[str, str]], state: str = 'EXECUTED') -> List[Dict[str, str]]:
+    """Функция возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению."""
+
+    result_filter_state = []
+
+    for find in find_state:
+        if find['state'] == state:
+            result_filter_state.append(find)
+
+    return result_filter_state
