273bc214b02e3d47dc98e500ea6ebbbb8110e11d
Denis <den23208@mail.ru>
2024-12-11 11:31:07 +0500

    correct fnc/add tests widget

--- src/masks.py
+++ src/masks.py
@@ -4,11 +4,11 @@ def get_mask_card_number(card_number: str) -> str:
 
     # Проверяем, что строка состоит только из цифр
     if not card_number.isdigit():
-        raise ValueError('Неверный формат карты. Номер карты должен содержать только цифры.')
+        raise ValueError("Неверный формат карты. Номер карты должен содержать только цифры.")
 
     # Проверяем длину номера карты
     if len(card_number) != 16:
-        raise ValueError('Неверная длина номера карты. Должно быть 16 цифр.')
+        raise ValueError("Неверная длина номера карты. Должно быть 16 цифр.")
 
     # Формируем маскированный номер карты
     masked_number = card_number[:4] + "-" + card_number[4:6] + "**-" + "****-" + card_number[-4:]
@@ -19,10 +19,10 @@ def get_mask_account(account_card: str) -> str:
     """Функция принимает на вход номер счёта и возвращает её в виде маски
     в формате **ХХХХ"""
     if not account_card.isdigit():
-        raise ValueError('Неверный формат счёта. Номер счёта должен содержать только цифры.')
+        raise ValueError("Неверный формат счёта. Номер счёта должен содержать только цифры.")
 
     if len(account_card) != 16:
-        raise ValueError('Неверная длина номера счёта. Должно быть 20 цифр.')
+        raise ValueError("Неверная длина номера счёта. Должно быть 20 цифр.")
 
     masks_account = "**" + account_card[-4:]
     return masks_account
--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,36 @@
-from typing import Tuple
-
-
-def mask_account_card(input_string: str) -> Tuple[str, str]:
+def mask_account_card(input_string: str) -> str:
     """Функция которая умеет обрабатывать информацию как о картах, так и о счетах"""
+    if not input_string:
+        raise ValueError("Входная строка не может быть пустой")
     digits = ""
     letters = ""
     for char in input_string:
         if char.isdigit():
             digits += char
-        elif char.isalpha():
+        elif char.isalpha() or char == " ":
             letters += char
+        else:
+            raise ValueError("Недопустимый символ при вводе карты.")
+    letters = " ".join(letters.split())
+    if len(digits) != 16 and len(digits) != 20:
+        raise ValueError("Неверная длина номера карты или счёта. Должно быть 16 цифр или 20 цифр.")
 
     if len(digits) == 16:
-        return letters, digits[:6] + "** ****" + digits[-4:]
+        return letters + " " + digits[:4] + " " + digits[4:6] + "** ****" + digits[-4:]
     elif len(digits) == 20:
-        return letters, "**" + digits[-4:]
+        return letters + " **" + digits[-4:]
     else:
-        return letters, digits
+        return ""
 
 
 def get_data(data: str) -> str:
-    date_str = data.split("T")[0]
-    year, month, day = date_str.split("-")
-    formatted_date = f"{day}.{month}.{year}"
-    return formatted_date
+    if len(data) == 26:
+        date_str = data.split("T")[0]
+        year, month, day = date_str.split("-")
+        formatted_date = f"{day}.{month}.{year}"
+        return formatted_date
+    else:
+        raise ValueError("Недопустимый формат даты и времени")
+
+
+print(get_data("2024-03-11T02:26:18.671407"))
